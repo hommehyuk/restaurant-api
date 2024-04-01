52913f20fb91e46b10d090691f7f9d49969df943
hommehyuk <bin6827@gmail.com>
2024-04-01 14:06:38 +0900

    Refresh Token 확인 로직

--- lib/user/view/login_screen.dart
+++ lib/user/view/login_screen.dart
@@ -25,11 +25,7 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     final dio = Dio();
 
-    // localhost
-    final emulatorIp = '10.0.2.2:3000';
-    final simulatorIp = '127.0.0.1:3000';
 
-    final ip = Platform.isIOS ? simulatorIp : emulatorIp;
 
     return DefaultLayout(
       child: SingleChildScrollView(
@@ -110,19 +106,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 ),
                 TextButton(
                   onPressed: () async {
-                    final refreshToken =
-                        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RAY29kZWZhY3RvcnkuYWkiLCJzdWIiOiJmNTViMzJkMi00ZDY4LTRjMWUtYTNjYS1kYTlkN2QwZDkyZTUiLCJ0eXBlIjoicmVmcmVzaCIsImlhdCI6MTcxMTY5MzA0NCwiZXhwIjoxNzExNzc5NDQ0fQ.0zGYgHmg9PXV0lmDag3wf_eFMioFKXFpVi5CcoTTr_g';
 
-                    final resp = await dio.post(
-                      'http://$ip/auth/token',
-                      options: Options(
-                        headers: {
-                          'authorization': 'Bearer $refreshToken',
-                        },
-                      ),
-                    );
-
-                    print(resp.data);
                   },
                   style: TextButton.styleFrom(
                     shape: RoundedRectangleBorder(
