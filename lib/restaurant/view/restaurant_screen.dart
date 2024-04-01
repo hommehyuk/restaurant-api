464680af5fe462c8f9fa9b6689c2225633d6c0f0
hommehyuk <bin6827@gmail.com>
2024-04-01 15:37:10 +0900

    JSON 데이터 매핑

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:restaurant_api/common/const/data.dart';
 import 'package:restaurant_api/restaurant/component/restaurant_card.dart';
+import 'package:restaurant_api/restaurant/model/restaurant_model.dart';
 
 class RestaurantScreen extends StatelessWidget {
   const RestaurantScreen({super.key});
@@ -39,22 +40,36 @@ class RestaurantScreen extends StatelessWidget {
               return ListView.separated(
                 itemBuilder: (_, index) {
                   final item = snapshot.data![index];
+                  // parsed
+                  final pItem = RestaurantModel(
+                    id: item['id'],
+                    name: item['name'],
+                    thumbUrl: 'http://$ip${item['thumbUrl']}',
+                    tags: List<String>.from(item['tags']),
+                    priceRange: RestaurantPriceRange.values.firstWhere(
+                      (e) => e.name == item['priceRange'],
+                    ),
+                    ratings: item['ratings'],
+                    ratingsCount: item['ratingsCount'],
+                    deliveryTime: item['deliveryTime'],
+                    deliveryFee: item['deliveryFee'],
+                  );
 
                   return RestaurantCard(
                     image: Image.network(
-                      'http://$ip${item['thumbUrl']}',
+                      pItem.thumbUrl,
                       fit: BoxFit.cover,
                     ),
                     // image: Image.asset(
                     //   'asset/img/food/ddeok_bok_gi.jpg',
                     //   fit: BoxFit.cover,
                     // ),
-                    name: item['name'],
-                    tags: List<String>.from(item['tags']),
-                    ratingsCount: item['ratingsCount'],
-                    deliveryTime: item['deliveryTime'],
-                    deliveryFee: item['deliveryFee'],
-                    ratings: item['ratings'],
+                    name: pItem.name,
+                    tags: pItem.tags,
+                    ratingsCount: pItem.ratingsCount,
+                    deliveryTime: pItem.deliveryTime,
+                    deliveryFee: pItem.deliveryFee,
+                    ratings: pItem.ratings,
                   );
                 },
                 separatorBuilder: (_, index) {
