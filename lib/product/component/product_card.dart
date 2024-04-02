7917defab84fb77953524663ad82523c36f075f8
hommehyuk <bin6827@gmail.com>
2024-04-02 14:20:50 +0900

    ProductCard 매핑

--- lib/product/component/product_card.dart
+++ lib/product/component/product_card.dart
@@ -1,8 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:restaurant_api/common/const/colors.dart';
+import 'package:restaurant_api/restaurant/model/restaurant_detail_model.dart';
 
 class ProductCard extends StatelessWidget {
-  const ProductCard({super.key});
+  final Image image;
+  final String name;
+  final String detail;
+  final int price;
+
+  const ProductCard({
+    required this.image,
+    required this.name,
+    required this.detail,
+    required this.price,
+    super.key,
+  });
+
+  factory ProductCard.fromModel({
+    required RestaurantProductModel model,
+  }) {
+    return ProductCard(
+      image: Image.network(
+        model.imgUrl,
+        width: 110,
+        height: 110,
+        fit: BoxFit.cover,
+      ),
+      name: model.name,
+      detail: model.detail,
+      price: model.price,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,12 +41,7 @@ class ProductCard extends StatelessWidget {
             borderRadius: BorderRadius.circular(
               8.0,
             ),
-            child: Image.asset(
-              'asset/img/food/ddeok_bok_gi.jpg',
-              width: 110,
-              height: 110,
-              fit: BoxFit.cover,
-            ),
+            child: image,
           ),
           SizedBox(width: 16.0),
           Expanded(
@@ -27,14 +50,14 @@ class ProductCard extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  '떡볶이',
+                  name,
                   style: TextStyle(
                     fontSize: 18.0,
                     fontWeight: FontWeight.w500,
                   ),
                 ),
                 Text(
-                  '전통 떡볶이의 정석!\n맛있습니다.',
+                  detail,
                   overflow: TextOverflow.ellipsis,
                   style: TextStyle(
                     color: BODY_TEXT_COLOR,
@@ -42,7 +65,7 @@ class ProductCard extends StatelessWidget {
                   ),
                 ),
                 Text(
-                  '₩10000',
+                  '₩$price',
                   textAlign: TextAlign.right,
                   style: TextStyle(
                     color: PRIMARY_COLOR,
