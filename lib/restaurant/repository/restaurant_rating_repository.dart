aa446256ca3d5996c0a2c286ddf3ff9497a8b24f
hommehyuk <bin6827@gmail.com>
2024-04-05 15:07:59 +0900

    RestaurantRatingProvider 초기 작업

--- lib/restaurant/repository/restaurant_rating_repository.dart
+++ lib/restaurant/repository/restaurant_rating_repository.dart
@@ -1,10 +1,21 @@
 import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:restaurant_api/common/const/data.dart';
+import 'package:restaurant_api/common/dio/dio.dart';
 import 'package:restaurant_api/common/model/cursor_pagination_model.dart';
 import 'package:restaurant_api/common/model/pagination_params.dart';
 import 'package:restaurant_api/rating/model/rating_model.dart';
 import 'package:retrofit/retrofit.dart';
 
 part 'restaurant_rating_repository.g.dart';
+
+final restaurantRatingRepositoryProvider =
+    Provider.family<RestaurantRatingRepository, String>((ref, id) {
+  final dio = ref.watch(dioProvider);
+
+  return RestaurantRatingRepository(dio,
+      baseUrl: 'http://$ip/restaurant/$id/rating');
+});
 
 // http://ip/restaurant/:rid/rating
 @RestApi()
